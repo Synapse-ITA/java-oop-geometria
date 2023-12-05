96b4210416cd1fe4fdb614eaa4e5138d24ee6ae9
Synapse-ITA <puggillovalerio@gmail.com>
2023-12-05 17:29:01 +0100

    Refactoring

--- src/org/lessons/java/geometria/Main.java
+++ src/org/lessons/java/geometria/Main.java
@@ -6,14 +6,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("SCEGLI LA BASE DEL TUO RETTANGOLO");
+        System.out.println("INSERISCI LA MISURA DELLA BASE DEL RETTANGOLO (in cm)");
         int newWidth = scanner.nextInt();
-        System.out.println("SCEGLI L'ALTEZZA DEL TUO RETTANGOLO");
+        System.out.println("INSERISCI LA MISURA DELL'ALTEZZA DEL RETTANGOLO (in cm)");
         int newHeight = scanner.nextInt();
 
         Rettangolo rettangoloUno = new Rettangolo(newWidth, newHeight);
-        System.out.println(rettangoloUno.calcoloArea());
-        System.out.println(rettangoloUno.calcoloPerimetro());
+        System.out.println("L'area del rettangolo è: " + rettangoloUno.calcoloArea() + "cm^2");
+        System.out.println("Il perimetro del rettangolo è: " + rettangoloUno.calcoloPerimetro() + "cm");
 
         scanner.close();
     }
